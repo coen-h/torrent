2047ece34073510d3d89e1123055921f5f8454f6
Coen <105825347+coen-h@users.noreply.github.com>
2026-05-14 15:04:36 +1200

    refinments

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 "use client";
 
+import { useEffect, useState } from 'react';
+import RefinementSelect from './components/RefinementSelect';
+import TypeSelect from './components/TypeSelect';
 import Script from 'next/script';
 
 export default function SoftwareSearch() {
+  
   return (
     <div className="mx-auto p-4">
-      <p className="text-4xl font-semibold bg-gradient-to-r from-neutral-200 to-emerald-300 bg-clip-text text-transparent">TEST ZITHER Font Test.</p>
+      <p className="text-4xl font-bold bg-gradient-to-r from-neutral-200 to-emerald-300 bg-clip-text text-transparent">TEST ZITHER Font Test.</p>
+
+      <div className='flex gap-2'>
+        <RefinementSelect />
+        <TypeSelect />
+      </div>
 
       <Script
         src="https://cse.google.com/cse.js?cx=a1c68bdb263434c9b"
