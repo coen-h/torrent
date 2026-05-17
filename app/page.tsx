666dfdb67fca316b796c5af46486c6107de64ad8
coen-h <105825347+coen-h@users.noreply.github.com>
2026-05-18 00:10:21 +1200

    need to remake to extract results

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,17 @@ import Script from 'next/script';
 export default function SoftwareSearch() {
   return (
     <div className="mx-auto p-4 flex flex-col items-center gap-2">
-      <div className="text-center space-y-2">
-        <h1 className="text-5xl md:text-6xl font-extrabold tracking-tight bg-gradient-to-br from-neutral-800 to-neutral-500 dark:from-neutral-200 dark:to-emerald-400 bg-clip-text text-transparent pb-1">Zither.</h1>
-        <p className="text-sm md:text-base text-neutral-500 dark:text-neutral-400 font-medium">The intelligent software discovery engine.</p>
+      <div className="text-center pb-1">
+        <h1 className="text-5xl font-extrabold tracking-tight bg-gradient-to-br from-neutral-200 to-emerald-400 bg-clip-text text-transparent pb-1">Zither.</h1>
+        <p className="text-neutral-500 dark:text-neutral-400 font-medium">The intelligent software discovery engine.</p>
       </div>
       
       <TypeLabels />
 
       <RefinementSelectors />
 
-      <hr className='bg-white text-white'/>
-
       <Script
-        src="https://cse.google.com/cse.js?cx=a1c68bdb263434c9b"
+        src="https://cse.google.com/cse.js?cx=e05ec25e76bc046f5"
         strategy="afterInteractive"
       />
 
