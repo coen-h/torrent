1d322c8b7d42d83f768f49ec798aac50f642a29d
coen-h <105825347+coen-h@users.noreply.github.com>
2026-05-27 23:53:23 +1200

    sidebar changes

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -3,10 +3,10 @@
 import { Gamepad, LaptopMinimal, Smartphone} from 'lucide-react';
 import Link from 'next/link';
 
-export default function SearchLayoutWrapper() {
+export default function Sidebar() {
   return (
-    <div className='h-full w-12 absolute left-0 top-0 p-1'>
-      <div className='bg-white/10 flex-col py-1 flex gap-2 items-center rounded'>
+    <div className='absolute h-full flex p-1 items-center justify-center top-0 left-0'>
+      <div className='bg-white/10 flex-col h-full w-10 py-1 flex gap-2 items-center rounded'>
         <Link href="/software" className='text-neutral-400 hover:text-white transition'><LaptopMinimal width={28} height={28} /></Link>
         <Link href="/games" className='text-neutral-400 hover:text-white transition'><Gamepad width={28} height={28} /></Link>
         <Link href="/apk" className='text-neutral-400 hover:text-white transition'><Smartphone width={28} height={28} /></Link>
